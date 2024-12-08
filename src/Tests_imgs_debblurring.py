e18af26c6da4d4a341a63a94074a4833b041d362
Farius AINA <ainaulrich@gmail.com>
2024-12-08 16:08:45 +0100

    Mise à jour des fichiers

---
+++ src/Tests_imgs_debblurring.py
@@ -0,0 +1,184 @@
+#import torch
+from Begin_Func import load_img, save_path, operateur, numpy_to_tensor, tensor_to_numpy, PSNR, search_opt, process_image_2
+from Proxy_Func import fista, prox_l6
+from Denoisers import DRUNet
+from Pnp_Algorithms import pnp_pgm, pnp_apgm
+import pandas as pd
+
+
+def main():
+
+    # # '''Chargement des images'''
+
+    Im_butterfly, Im_leaves, Im_starfish = load_img("butterfly.png"), load_img("leaves.png"), load_img("starfish.png")
+
+
+    # # # Proxy Algorithms
+
+
+    # # '''Test Défloutage '''
+
+    sigma_low_2, sigma_mod_2, sigma_high_2 = (1, 1), (2, 2), (3, 3)
+
+    Im_butterfly_blurred_low, G_butterfly_low = operateur(Im_butterfly).blur(sigma = sigma_low_2, angle = 0)
+    
+    Im_butterfly_blurred_mod, G_butterfly_mod = operateur(Im_butterfly).blur(sigma = sigma_mod_2, angle = 0)
+
+    Im_butterfly_blurred_high, G_butterfly_high = operateur(Im_butterfly).blur(sigma = sigma_high_2, angle = 0)
+    
+    Im_leaves_blurred_low, G_leaves_low = operateur(Im_leaves).blur(sigma = sigma_low_2, angle = 0)
+    
+    Im_leaves_blurred_mod, G_leaves_mod = operateur(Im_leaves).blur(sigma = sigma_mod_2, angle = 0)
+
+    Im_leaves_blurred_high, G_leaves_high = operateur(Im_leaves).blur(sigma = sigma_high_2, angle = 0)
+
+    Im_starfish_blurred_low, G_starfish_low = operateur(Im_starfish).blur(sigma = sigma_low_2, angle = 0)
+    
+    Im_starfish_blurred_mod, G_starfish_mod = operateur(Im_starfish).blur(sigma = sigma_mod_2, angle = 0)
+
+    Im_starfish_blurred_high, G_starfish_high = operateur(Im_starfish).blur(sigma = sigma_high_2, angle = 0)
+
+
+    Im_butterfly_deblurred_low, Tb_low = fista(Im_butterfly_blurred_low, "convolution", {"G": G_butterfly_low}, 1e-4, 1, 25, prox=prox_l6, prox_params={"tau": 1e-4, "K": 20}, tol=1e-7)
+
+    Im_butterfly_deblurred_mod, Tb_mod = fista(Im_butterfly_blurred_mod, "convolution", {"G": G_butterfly_mod}, 1e-4, 1, 30, prox=prox_l6, prox_params={"tau": 1e-4, "K": 20}, tol=1e-7)
+
+    Im_butterfly_deblurred_high, Tb_high = fista(Im_butterfly_blurred_high, "convolution", {"G": G_butterfly_high}, 1e-4, 1.3, 200, prox=prox_l6, prox_params={"tau": 0.01, "K": 5}, tol=1e-7)
+    
+    Im_leaves_deblurred_low, Tl_low = fista(Im_leaves_blurred_low, "convolution", {"G": G_leaves_low}, 1e-4, 1, 25, prox=prox_l6, prox_params={"tau": 1e-4, "K": 20}, tol=1e-7)
+
+    Im_leaves_deblurred_mod, Tl_mod = fista(Im_leaves_blurred_mod, "convolution", {"G": G_leaves_mod}, 1e-4, 1, 30, prox=prox_l6, prox_params={"tau": 1e-4, "K": 20}, tol=1e-7)
+
+    Im_leaves_deblurred_high, Tl_high = fista(Im_leaves_blurred_high, "convolution", {"G": G_leaves_high}, 1e-4, 1.3, 200, prox=prox_l6, prox_params={"tau": 0.01, "K": 5}, tol=1e-7)
+
+    Im_starfish_deblurred_low, Ts_low = fista(Im_starfish_blurred_low, "convolution", {"G": G_starfish_low}, 1e-4, 1, 25, prox=prox_l6, prox_params={"tau": 1e-4, "K": 20}, tol=1e-7)
+
+    Im_starfish_deblurred_mod, Ts_mod = fista(Im_starfish_blurred_mod, "convolution", {"G": G_starfish_mod}, 1e-4, 1, 30, prox=prox_l6, prox_params={"tau": 1e-4, "K": 20}, tol=1e-7)
+
+    Im_starfish_deblurred_high, Ts_high = fista(Im_starfish_blurred_high, "convolution", {"G": G_starfish_high}, 1e-4, 1.3, 200, prox=prox_l6, prox_params={"tau": 0.01, "K": 5}, tol=1e-7)
+
+    # print(PSNR(Im_leaves, Im_leaves_deblurred_high, 1.0))
+
+
+    
+    # # # PNP(Plug and Play Algorithms)
+
+    # # PNP_Défloutage
+
+    denoiser = DRUNet()
+
+    Im_butterfly_deblurred_low_pnp, Tb_low_pnp = pnp_pgm(Im_butterfly_blurred_low, "convolution", {"G": G_butterfly_low}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+
+    Im_butterfly_deblurred_mod_pnp, Tb_mod_pnp = pnp_pgm(Im_butterfly_blurred_mod, "convolution", {"G": G_butterfly_mod}, 2, denoiser, sigma=1e-1, K=48, tol=1e-7)
+
+    Im_butterfly_deblurred_high_pnp, Tb_high_pnp = pnp_pgm(Im_butterfly_blurred_high, "convolution", {"G": G_butterfly_high}, 2, denoiser, sigma= 1e-1, K=48, tol=1e-7)
+
+    Im_leaves_deblurred_low_pnp, Tl_low_pnp = pnp_pgm(Im_leaves_blurred_low, "convolution", {"G": G_leaves_low}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+
+    Im_leaves_deblurred_mod_pnp, Tl_mod_pnp = pnp_pgm(Im_leaves_blurred_mod, "convolution", {"G": G_leaves_mod}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+
+    Im_leaves_deblurred_high_pnp, Tl_high_pnp = pnp_pgm(Im_leaves_blurred_high, "convolution", {"G": G_leaves_high}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+    
+    Im_starfish_deblurred_low_pnp, Ts_low_pnp = pnp_pgm(Im_starfish_blurred_low, "convolution", {"G": G_starfish_low}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+
+    Im_starfish_deblurred_mod_pnp, Ts_mod_pnp = pnp_pgm(Im_starfish_blurred_mod, "convolution", {"G": G_starfish_mod}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+
+    Im_starfish_deblurred_high_pnp, Ts_high_pnp = pnp_pgm(Im_starfish_blurred_high, "convolution", {"G": G_starfish_high}, 2, denoiser, sigma=1e-3, K=48, tol=1e-7)
+
+    # print(PSNR(Im_starfish, Im_starfish_deblurred_high_pnp, 1.0))
+
+    
+    # # # '''Recherche paramètres optimaux (Proxy and PNP)'''
+    
+    # Proxy
+
+    # param_ranges = { "K": [10, 20, 30], "lambd": [0.0001, 0.01, 0.1, 1.3], "tau": [0.0001, 0.01, 0.1, 1.3]}
+
+    # prox_params_ranges = {"tau": [0.0001, 0.01, 0.1, 1.3],  "K": [10, 20, 30]}
+
+    # func_params = {"u": Im_leaves_blurred_high, "operator_type": "convolution", "operator_params": {"G": G_leaves_high}, "prox": prox_l6, "tol": 1e-7}
+
+    # best_params, best_score, score_map = search_opt(func=fista, u_truth=Im_leaves, param_ranges=param_ranges, 
+    # metric=PSNR, func_params=func_params, prox_params_ranges=prox_params_ranges)
+
+
+    # # PNP
+
+    # param_ranges_pnp = { "K": [15, 25, 50], "tau": [0.25, 0.5, 1], "sigma": [sigma_low, sigma_mod, sigma_high]}
+
+    # func_params_pnp = {"u": Im_leaves_blurred_high, "operator_type": "convolution", "operator_params": {"G": G_leaves_high}, "denoiser": denoiser, "tol": 1e-7}
+
+    # best_params, best_score, score_map = search_opt(func=pnp_apgm, u_truth=Im_leaves, param_ranges=param_ranges_pnp, metric=PSNR, func_params=func_params_pnp)
+
+    # # Résultats
+
+    # print("Meilleurs paramètres globaux:", best_params)
+    # print("Meilleur score:", best_score)
+    # print(score_map.head())
+
+
+    # # # Save images 
+
+    # # Images Deblurring
+
+    reference_image_list = [Im_butterfly, Im_leaves, Im_starfish]
+
+    names_deblurred_sig_1 = [["Low Blur B ($\sigma=1$)", "TV Deblurring B ($\sigma=1$)", "PNP_DRUNet Deblurring B ($\sigma=1$)"], 
+                                ["Low Blur L ($\sigma=1$)", "TV Deblurring L ($\sigma=1$)", "PNP_DRUNet Deblurring L ($\sigma=1$)"], 
+                                ["Low Blur S ($\sigma=1$)", "TV Deblurring S ($\sigma=1$)", "PNP_DRUNet Deblurring S ($\sigma=1$)"]]
+
+    images_deblurred_sig_1 =[[Im_butterfly_blurred_low, Im_butterfly_deblurred_low, Im_butterfly_deblurred_low_pnp], 
+                                [Im_leaves_blurred_low, Im_leaves_deblurred_low, Im_leaves_deblurred_low_pnp], 
+                                [Im_starfish_blurred_low, Im_starfish_deblurred_low, Im_starfish_deblurred_low_pnp]]
+
+    deblur_trajectories_sig_1 = [[Tb_low, Tb_low_pnp], [Tl_low, Tl_low_pnp], [Ts_low, Ts_low_pnp]]
+
+
+    names_deblurred_sig_2 = [["Moderate Blur B ($\sigma=2$)", "TV Deblurring B ($\sigma=2$)", "PNP_DRUNet Deblurring B ($\sigma=2$)"], 
+                                ["Moderate Blur L ($\sigma=2$)", "TV Deblurring L ($\sigma=2$)", "PNP_DRUNet Deblurring L ($\sigma=2$)"], 
+                                ["Moderate Blur S ($\sigma=2$)", "TV Deblurring S ($\sigma=2$)", "PNP_DRUNet Deblurring S ($\sigma=2$)"]]    
+
+    images_deblurred_sig_2 =[[Im_butterfly_blurred_mod, Im_butterfly_deblurred_mod, Im_butterfly_deblurred_mod_pnp], 
+                                [Im_leaves_blurred_mod, Im_leaves_deblurred_mod, Im_leaves_deblurred_mod_pnp], 
+                                [Im_starfish_blurred_mod, Im_starfish_deblurred_mod, Im_starfish_deblurred_mod_pnp]]
+
+    deblur_trajectories_sig_2 = [[Tb_mod, Tb_mod_pnp], [Tl_mod, Tl_mod_pnp], [Ts_mod, Ts_mod_pnp]]
+
+
+    names_deblurred_sig_3 = [["High Blur B ($\sigma=3$)", "TV Deblurring B ($\sigma=3$)", "PNP_DRUNet Deblurring B ($\sigma=3$)"], 
+                                ["High Blur L ($\sigma=3$)", "TV Deblurring L ($\sigma=3$)", "PNP_DRUNet Deblurring L ($\sigma=3$)"], 
+                                ["High Blur S ($\sigma=3$)", "TV Deblurring S ($\sigma=3$)", "PNP_DRUNet Deblurring S ($\sigma=3$)"]]    
+
+    images_deblurred_sig_3 =[[Im_butterfly_blurred_high, Im_butterfly_deblurred_high, Im_butterfly_deblurred_high_pnp], 
+                                [Im_leaves_blurred_high, Im_leaves_deblurred_high, Im_leaves_deblurred_high_pnp], 
+                                [Im_starfish_blurred_high, Im_starfish_deblurred_high, Im_starfish_deblurred_high_pnp]]
+
+    deblur_trajectories_sig_3 = [[Tb_high, Tb_high_pnp], [Tl_high, Tl_high_pnp], [Ts_high, Ts_high_pnp]]
+
+    save_path("results", "Imgs_deblurred_sig_1", names_deblurred_sig_1, images_deblurred_sig_1, reference_image_list=reference_image_list, 
+              psnr=True, trajectories_list=deblur_trajectories_sig_1)
+    
+    save_path("results", "Imgs_deblurred_sig_2", names_deblurred_sig_2, images_deblurred_sig_2, reference_image_list=reference_image_list, 
+              psnr=True, trajectories_list=deblur_trajectories_sig_2)
+
+    save_path("results", "Imgs_deblurred_sig_3", names_deblurred_sig_3, images_deblurred_sig_3, reference_image_list=reference_image_list, 
+              psnr=True, trajectories_list=deblur_trajectories_sig_3)
+
+
+    # import matplotlib.pyplot as plt
+    # plt.figure(figsize=(18, 6), dpi=200)
+    # plt.subplot(131)
+    # plt.imshow(Im_butterfly_blurred_high)
+    # plt.subplot(132)
+    # plt.imshow(G_butterfly_high)
+    # plt.subplot(133)
+    # plt.imshow(Im_butterfly_deblurred_low_pnp)
+
+    # print([PSNR(Im_butterfly, Im_butterfly_deblurred_high_pnp),
+    #        PSNR(Im_leaves, Im_leaves_deblurred_high_pnp),
+    #         PSNR(Im_starfish, Im_starfish_deblurred_high_pnp)])
+
+
+if __name__ == "__main__" :
+
+    main()
---
+++ src/Tests_imgs_denoising.py
@@ -0,0 +1,169 @@
+#import torch
+from Begin_Func import load_img, save_path, operateur, numpy_to_tensor, tensor_to_numpy, PSNR, search_opt, process_image_2
+from Proxy_Func import fista, prox_l6
+from Denoisers import DRUNet
+from Pnp_Algorithms import pnp_pgm, pnp_apgm
+import pandas as pd
+
+def main():
+
+    # # '''Chargement des images'''
+
+    Im_butterfly, Im_leaves, Im_starfish = load_img("butterfly.png"), load_img("leaves.png"), load_img("starfish.png")
+
+
+    # # # Proxy Algorithms
+
+    # # '''Test Bruitage'''
+
+    sigma_low, sigma_mod, sigma_high = 0.06, 0.1, 0.2
+
+    Im_butterfly_noised_sig15, Im_butterfly_noised_sig25, Im_butterfly_noised_sig50 = operateur(Im_butterfly).noise(sigma=sigma_low), operateur(Im_butterfly).noise(sigma=sigma_mod), operateur(Im_butterfly).noise(sigma=sigma_high)
+
+    Im_leaves_noised_sig15, Im_leaves_noised_sig25, Im_leaves_noised_sig50 = operateur(Im_leaves).noise(sigma=sigma_low), operateur(Im_leaves).noise(sigma=sigma_mod), operateur(Im_leaves).noise(sigma=sigma_high)
+    
+    Im_starfish_noised_sig15, Im_starfish_noised_sig25, Im_starfish_noised_sig50 = operateur(Im_starfish).noise(sigma=sigma_low), operateur(Im_starfish).noise(sigma=sigma_mod), operateur(Im_starfish).noise(sigma=sigma_high)
+
+    Im_butterfly_denoised_sig15, Tb15 = fista(Im_butterfly_noised_sig15, "none", None, 0.01, 0.25, 5, prox=prox_l6, prox_params={"tau": 0.1, "K": 5}, tol=1e-7)
+
+    Im_butterfly_denoised_sig25, Tb25 = fista(Im_butterfly_noised_sig25, "none", None, 0.01, 0.01, 5, prox=prox_l6, prox_params={"tau": 0.1, "K": 20}, tol=1e-7)
+
+    Im_butterfly_denoised_sig50, Tb50 = fista(Im_butterfly_noised_sig50, "none", None, 0.01, 0.01, 9, prox=prox_l6, prox_params={"tau": 0.1, "K": 20}, tol=1e-7)
+
+    Im_leaves_denoised_sig15, Tl15 = fista(Im_leaves_noised_sig15, "none", None, 0.01, 0.25, 5, prox=prox_l6, prox_params={"tau": 0.1, "K": 5}, tol=1e-7)
+
+    Im_leaves_denoised_sig25, Tl25 = fista(Im_leaves_noised_sig25, "none", None, 0.01, 0.01, 5, prox=prox_l6, prox_params={"tau": 0.1, "K": 20}, tol=1e-7)
+
+    Im_leaves_denoised_sig50, Tl50 = fista(Im_leaves_noised_sig50, "none", None, 0.01, 0.01, 9, prox=prox_l6, prox_params={"tau": 0.1, "K": 20}, tol=1e-7)
+
+    Im_starfish_denoised_sig15, Ts15 = fista(Im_starfish_noised_sig15, "none", None, 0.01, 0.25, 5, prox=prox_l6, prox_params={"tau": 0.1, "K": 5}, tol=1e-7)
+
+    Im_starfish_denoised_sig25, Ts25 = fista(Im_starfish_noised_sig25, "none", None, 0.01, 0.01, 5, prox=prox_l6, prox_params={"tau": 0.1, "K": 20}, tol=1e-7)
+
+    Im_starfish_denoised_sig50, Ts50 = fista(Im_starfish_noised_sig50, "none", None, 0.01, 0.01, 9, prox=prox_l6, prox_params={"tau": 0.1, "K": 20}, tol=1e-7)
+
+    # print(PSNR(Im_butterfly, Im_butterfly_denoised_sig50, 1.0))
+
+    
+    # # # PNP(Plug and Play Algorithms)
+
+    # # PNP_Débruiteur (Im_butterfly)
+
+    denoiser = DRUNet()
+
+    Im_butterfly_denoised_sig15_pnp, Tb15_pnp = pnp_apgm(Im_butterfly_noised_sig15, "none", None, 1, denoiser, sigma=sigma_low, K=5, tol=1e-7)
+
+    Im_butterfly_denoised_sig25_pnp, Tb25_pnp = pnp_apgm(Im_butterfly_noised_sig25, "none", None, 1, denoiser, sigma=sigma_mod, K=5, tol=1e-7)
+
+    Im_butterfly_denoised_sig50_pnp, Tb50_pnp = pnp_pgm(Im_butterfly_noised_sig50, "none", None, 1, denoiser, sigma=sigma_high, K=15, tol=1e-7)
+
+    Im_leaves_denoised_sig15_pnp, Tl15_pnp = pnp_apgm(Im_leaves_noised_sig15, "none", None, 1, denoiser, sigma=sigma_low, K=5, tol=1e-7)
+
+    Im_leaves_denoised_sig25_pnp, Tl25_pnp = pnp_apgm(Im_leaves_noised_sig25, "none", None, 1, denoiser, sigma=sigma_mod, K=5, tol=1e-7)
+
+    Im_leaves_denoised_sig50_pnp, Tl50_pnp = pnp_pgm(Im_leaves_noised_sig50, "none", None, 1, denoiser, sigma=sigma_high, K=15, tol=1e-7)
+
+    Im_starfish_denoised_sig15_pnp, Ts15_pnp = pnp_apgm(Im_starfish_noised_sig15, "none", None, 1, denoiser, sigma=sigma_low, K=5, tol=1e-7)
+
+    Im_starfish_denoised_sig25_pnp, Ts25_pnp = pnp_apgm(Im_starfish_noised_sig25, "none", None, 1, denoiser, sigma=sigma_mod, K=5, tol=1e-7)
+
+    Im_starfish_denoised_sig50_pnp, Ts50_pnp = pnp_pgm(Im_starfish_noised_sig50, "none", None, 1, denoiser, sigma=sigma_high, K=15, tol=1e-7)
+
+    # print(PSNR(Im_butterfly, Im_butterfly_denoised_sig50_pnp, 1.0))
+
+
+    # # # '''Recherche paramètres optimaux (Proxy and PNP)'''
+    
+    # # Proxy
+
+    # param_ranges = { "K": [5, 10, 15, 20], "lambd": [0.01, 0.1, 0.5], "tau": [0.01, 0.1, 0.5]}
+
+    # prox_params_ranges = {"tau": [0.01, 0.1, 0.5],  "K": [5, 10, 15, 20]}
+
+    # func_params = {"u": Im_butterfly_noised_sig50, "operator_type": "none", "operator_params": {}, "prox": prox_l6, "tol": 1e-7}
+
+    # best_params, best_score, score_map = search_opt(func=fista, u_truth=Im_butterfly, param_ranges=param_ranges, 
+    # metric=PSNR, func_params=func_params, prox_params_ranges=prox_params_ranges)
+
+
+    # # PNP
+
+    # param_ranges_pnp = { "K": [5, 10, 15], "tau": [0.01, 0.1, 1], "sigma": [sigma_low, sigma_mod, sigma_high]}
+    
+    # func_params_pnp = {"u": Im_leaves_noised_sig50, "operator_type": "none", "operator_params": {}, "denoiser": denoiser, "tol": 1e-7}
+
+    # best_params, best_score, score_map = search_opt(func=pnp_apgm, u_truth=Im_leaves, param_ranges=param_ranges_pnp, metric=PSNR, func_params=func_params_pnp)
+
+
+    # # Résultats
+
+    # print("Meilleurs paramètres globaux:", best_params)
+    # print("Meilleur score:", best_score)
+    # print(score_map.head())
+
+
+    # # # Save images 
+    
+    # # Images Denoising
+
+    reference_image_list = [Im_butterfly, Im_leaves, Im_starfish]
+
+    names_denoised_sig15 = [["Low Noise B ($\sigma=0.06$)", "TV Denoising B ($\sigma=0.06$)", "PNP_DRUNet Denoising B ($\sigma=0.06$)"], 
+                            ["Low Noise L ($\sigma=0.06$)", "TV Denoising L ($\sigma=0.06$)", "PNP_DRUNet Denoising L ($\sigma=0.06$)"], 
+                            ["Low Noise S ($\sigma=0.06$)", "TV Denoising S ($\sigma=0.06$)", "PNP_DRUNet Denoising S ($\sigma=0.06$)"]]
+
+    images_denoised_sig15 =[[Im_butterfly_noised_sig15, Im_butterfly_denoised_sig15, Im_butterfly_denoised_sig15_pnp],
+                            [Im_leaves_noised_sig15, Im_leaves_denoised_sig15, Im_leaves_denoised_sig15_pnp],
+                            [Im_starfish_noised_sig15, Im_starfish_denoised_sig15, Im_starfish_denoised_sig15_pnp]]
+
+    denoise_trajectories_sig_15 = [[Tb15, Tb15_pnp], [Tl15, Tl15_pnp], [Ts15, Ts15_pnp]]
+
+    names_denoised_sig25 = [["Moderate Noise B ($\sigma=0.1$)", "TV Denoising B ($\sigma=0.1$)", "PNP_DRUNet Denoising B ($\sigma=0.1$)"], 
+                            ["Moderate Noise L ($\sigma=0.1$)", "TV Denoising L ($\sigma=0.1$)", "PNP_DRUNet Denoising L ($\sigma=0.1$)"], 
+                            ["Moderate Noise S ($\sigma=0.1$)", "TV Denoising S ($\sigma=0.1$)", "PNP_DRUNet Denoising S ($\sigma=0.1$)"]]
+
+    images_denoised_sig25 =[[Im_butterfly_noised_sig25, Im_butterfly_denoised_sig25, Im_butterfly_denoised_sig25_pnp],
+                            [Im_leaves_noised_sig25, Im_leaves_denoised_sig25, Im_leaves_denoised_sig25_pnp],
+                            [Im_starfish_noised_sig25, Im_starfish_denoised_sig25, Im_starfish_denoised_sig25_pnp]]
+
+    denoise_trajectories_sig_25 = [[Tb25, Tb25_pnp], [Tl25, Tl25_pnp], [Ts25, Ts25_pnp]]
+
+    names_denoised_sig50 = [["High Noise B ($\sigma=0.2$)", "TV Denoising B ($\sigma=0.2$)", "PNP_DRUNet Denoising B ($\sigma=0.2$)"], 
+                            ["High Noise L ($\sigma=0.2$)", "TV Denoising L ($\sigma=0.2$)", "PNP_DRUNet Denoising L ($\sigma=0.2$)"], 
+                            ["High Noise S ($\sigma=0.2$)", "TV Denoising S ($\sigma=0.2$)", "PNP_DRUNet Denoising S ($\sigma=0.2$)"]]
+
+    images_denoised_sig50 =[[Im_butterfly_noised_sig50, Im_butterfly_denoised_sig50, Im_butterfly_denoised_sig50_pnp],
+                            [Im_leaves_noised_sig50, Im_leaves_denoised_sig50, Im_leaves_denoised_sig50_pnp],
+                            [Im_starfish_noised_sig50, Im_starfish_denoised_sig50, Im_starfish_denoised_sig50_pnp]]
+
+    denoise_trajectories_sig_50 = [[Tb50, Tb50_pnp], [Tl50, Tl50_pnp], [Ts50, Ts50_pnp]]
+
+
+    save_path("results", "Imgs_denoised_sig15", names_denoised_sig15, images_denoised_sig15, reference_image_list= reference_image_list, 
+              psnr=True, trajectories_list=denoise_trajectories_sig_15)
+    
+    save_path("results", "Imgs_denoised_sig25", names_denoised_sig25, images_denoised_sig25, reference_image_list=reference_image_list, 
+              psnr=True, trajectories_list=denoise_trajectories_sig_25)
+
+    save_path("results", "Imgs_denoised_sig50", names_denoised_sig50, images_denoised_sig50, reference_image_list=reference_image_list, 
+              psnr=True, trajectories_list=denoise_trajectories_sig_50)
+
+
+    # import numpy as np
+    # import matplotlib.pyplot as plt
+
+    # plt.figure(figsize=(18, 6), dpi=200)
+    # plt.subplot(131)
+    # plt.imshow(Im_butterfly_denoised_sig50_pnp)
+    # plt.subplot(132)
+    # plt.imshow(Im_leaves_denoised_sig50_pnp)
+    # plt.subplot(133)
+    # plt.imshow(Im_starfish_denoised_sig50_pnp)
+
+    # print([PSNR(Im_butterfly, Im_butterfly_denoised_sig50_pnp),
+    #        PSNR(Im_leaves, Im_leaves_denoised_sig50_pnp),
+    #         PSNR(Im_starfish, Im_starfish_denoised_sig50_pnp)])
+
+if __name__ == "__main__" :
+
+    main()
